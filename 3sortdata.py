994a8320993d87f7808d85ea861ee22d69e060da
Hao Chen <chenhao@act.buaa.edu.cn>
2019-06-06 18:45:07 +0800

    '啥也没干'

--- 3sortdata.py
+++ 3sortdata.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # 考虑这里开始分 窗口 把time drop掉重新设time！
 os.chdir('data')
-data = pd.read_csv('raw_data.csv',nrows=600000)
+data = pd.read_csv('raw_data.csv')
 data.sort_values('time', kind='mergesort', inplace=True)
 
 
@@ -17,6 +17,7 @@ def fun(group):
     return group
 
 
+# ----------label,顺便改变时间之后就可以设置不同的窗口了
 # label = data.groupby(['time']).apply(
 #
 #     lambda group: Counter(group.label).most_common(1)[0][0]
@@ -32,4 +33,4 @@ data = data.groupby(['time']).apply(
 print(data.shape)
 
 # label.to_csv('label.csv', index=False, header=['label'])
-data.to_csv('data_sorted_filter.csv', index=False)
+data.to_csv('data_sorted_filter.csv', chunksize=60000, index=False)
--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 
 def draw():
@@ -7,12 +8,13 @@ def draw():
     data = pd.read_csv('data_sorted.csv')
     print(data.shape)
     # plt.figure()
-    data1 = data[data['label'] == 6]
-    # data1 = data1[data['acc_x'] > 40]
-    data1 = data1[['acc_x', 'acc_y', 'acc_z']]
-    ax = data1.plot(kind='line')
+    data1 = data[data['label'] == 4]
+    # data1 = data1[data.values < 40]
+    data1 = data1.drop(['time', 'label', 'pressure'], axis=1)
+    print(data1.shape)
+    ax = data1.plot(kind='line', figsize=(30, 10))
     fig = ax.get_figure()
-    fig.savefig("6_acc.png")
+    fig.savefig("t_1all.png")
 
 
 if __name__ == '__main__':
